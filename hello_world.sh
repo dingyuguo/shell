29a76dcb7ea569b93ef06042ee2d06fd9f0c0b7d
dyg <dyg1993@foxmail.com>
2015-10-23 16:45:42 +0800

    hello world

---
+++ hello_world.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: hello_world.sh
+# Author: laoding
+# mail: dyg1993@foxmail.com
+# Created Time: Fri 23 Oct 2015 04:45:02 PM CST
+#########################################################################
+#!/bin/bash
+echo hello.sh
+
